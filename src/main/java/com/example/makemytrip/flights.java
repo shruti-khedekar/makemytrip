e7ea9e8295381fc2315250691efb096c5a9b7d19
Tejankar <atejankar@deloitte.com>
2023-10-15 09:54:38 +0530

    codeRefractor

---
+++ src/main/java/com/example/makemytrip/flights.java
@@ -0,0 +1,12 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flights {
+    @GetMapping("/flights")
+    public String getData() {return " welcome to flight booking " ; }
+
+
+}
