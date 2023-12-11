d29e39d9bc5e87d4b59897e8336a8b8fb2104929
shruti-khedekar <49498398+shruti-khedekar@users.noreply.github.com>
2023-12-11 18:29:02 +0530

    Update flights.java

--- src/main/java/com/example/makemytrip/flights.java
+++ src/main/java/com/example/makemytrip/flights.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flights {
     @GetMapping("/flights")
-    public String getData() {return " welcome to flight booking at 10 % discount " ; }
+    public String getData() {return " welcome to flight booking at 10 % discount on flights" ; }
 
 
 }
